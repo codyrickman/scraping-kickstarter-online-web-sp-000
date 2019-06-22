b29394259f48e95f7b03fc53587f33533fa99f0d
Cody Rickman <cody@creativemindstudios.com>
2019-06-22 17:00:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   project_hash = {}
-  kickstarter.css("li.project.grid_4").each |project| do
+  kickstarter.css("li.project.grid_4").each do |project|
     project_hash << project.css("h2.bbcard_name strong a").text
   end
   puts project_hash
