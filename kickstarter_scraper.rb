ffa7fb313a2b20db765666c3616924875af2d689
Cody Rickman <cody@creativemindstudios.com>
2019-06-22 17:06:03 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,6 +9,7 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |project|
     projects << project.css("h2.bbcard_name strong a").text
   end
+  puts projects
   puts project_hash
 end
 
